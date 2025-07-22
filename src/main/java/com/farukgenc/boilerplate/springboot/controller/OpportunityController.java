8fd731e856673b899e700b69766facb40c6f4792
Trần Mạnh Hưng <tranhung26122612@gmail.com>
2025-07-22 11:18:51 +0700

    feat: refactor

--- src/main/java/com/farukgenc/boilerplate/springboot/controller/OpportunityController.java
+++ src/main/java/com/farukgenc/boilerplate/springboot/controller/OpportunityController.java
@@ -7,6 +7,7 @@ import com.farukgenc.boilerplate.springboot.security.dto.request.CreateOpportuni
 import com.farukgenc.boilerplate.springboot.security.dto.response.RegistrationResponse;
 import com.farukgenc.boilerplate.springboot.security.dto.request.SearchOpportunityRequest;
 import com.farukgenc.boilerplate.springboot.security.service.OpportunityService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,13 +24,14 @@ public class OpportunityController {
     private final OpportunityService opportunityService;
 
     @PostMapping("/register")
+    @Operation(tags = "Opportunity Service", description = "You can register new opportunity by sending information in the appropriate format.")
     public ResponseEntity<RegistrationResponse> createNewOpportunity(@Valid @RequestBody CreateOpportunityRequest request) {
-
         final RegistrationResponse createOpportunityResponse = opportunityService.createNewOpportunity(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createOpportunityResponse);
     }
 
     @PostMapping("/search")
+    @Operation(tags = "Opportunity Service", description = "You can search opportunities.")
     public ResponseEntity<CustomPage<Opportunity>> searchOpportunity(@Valid @RequestBody SearchOpportunityRequest request) {
         CustomPage<Opportunity> opportunityCustomPage = opportunityService.searchOpportunity(request);
         return ResponseEntity.status(HttpStatus.OK).body(opportunityCustomPage);
