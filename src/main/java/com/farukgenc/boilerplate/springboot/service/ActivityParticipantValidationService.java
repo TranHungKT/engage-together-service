5244b8d0a947da73794898c94086f8891729e189
Trần Mạnh Hưng <tranhung26122612@gmail.com>
2025-08-27 11:32:01 +0700

    feat: update docker volumn for postgre image

--- src/main/java/com/farukgenc/boilerplate/springboot/service/ActivityParticipantValidationService.java
+++ src/main/java/com/farukgenc/boilerplate/springboot/service/ActivityParticipantValidationService.java
@@ -1,13 +1,11 @@
 package com.farukgenc.boilerplate.springboot.service;
 
 import com.farukgenc.boilerplate.springboot.exceptions.RegistrationException;
-import com.farukgenc.boilerplate.springboot.model.ActivityParticipant;
 import com.farukgenc.boilerplate.springboot.repository.projections.ParticipantProjection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,9 +13,9 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class ActivityParticipantValidationService {
-    public void validateParticipantAlreadyJoin(UUID userId, List<ParticipantProjection> activityParticipants){
+    public void validateParticipantAlreadyJoin(UUID userId, List<ParticipantProjection> activityParticipants) {
         var activityParticipantIds = activityParticipants.stream().map(ParticipantProjection::getUserId).toList();
-        if(activityParticipantIds.contains(userId)){
+        if (activityParticipantIds.contains(userId)) {
             log.warn("User has already join this activity");
 
             throw new RegistrationException("User has already join this activity");
